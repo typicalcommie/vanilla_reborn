28d8ec7f7cc90edbe6cf3930da73d8c79ae9653d
typicalcommie <typicalcommie@yandex.ru>
2021-10-04 17:29:07 +0300

    Initial commit

---
+++ common/defines/changes_defines.lua
@@ -0,0 +1,19 @@
+-- Country
+NDefines.NCountry.EVENT_PROCESS_OFFSET = 30
+NDefines.NCountry.STATE_VALUE_NON_CORE_STATE_FRACTION = 1
+NDefines.NCountry.SPECIAL_FORCES_CAP_BASE = 0
+NDefines.NCountry.BASE_MAX_COMMAND_POWER = 1000
+NDefines.NProduction.CAPITAL_SHIP_MAX_NAV_FACTORIES_PER_LINE = 30
+-- Buildings
+NDefines.NProduction.MAX_CIV_FACTORIES_PER_LINE = 20
+-- Focuses
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 30
+-- Air Combat
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 5000 -- Max army experience a country can store
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 5000 -- Max navy experience a country can store
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 5000  -- Max air experience a country can store
+NDefines.NAir.AIR_WING_MAX_STATS_BOMBING = 5000
+NDefines.NAir.STRATEGIC_BOMBER_NUKE_AIR_SUPERIORITY = 0.4
+-- Politics
+NDefines.NDiplomacy.BASE_SEND_ATTACHE_CP_COST = 50
+
---
+++ descriptor.mod
@@ -0,0 +1,10 @@
+version="*"
+tags={
+	"Balance"
+	"National Focuses"
+	"Technologies"
+	"Translation"
+	"Fixes"
+}
+name="Vanilla Reborn"
+supported_version="1.10.8"
